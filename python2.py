ed9a4c63f3f8d028d1d66ef908c2c5f258a1efa7
xdhia7 <xdhia7@gmail.com>
2023-11-29 20:45:02 +0300

    main

---
+++ python2.py
@@ -0,0 +1,13 @@
+def main():
+    number = RandomNumber()
+    guess = input("Enter a number")
+    CheckUserChoice(number, guess)
+    return None 
+
+def RandomNumber():
+    return None 
+
+def CheckUserChoice(number, guess):
+    return None
+
+main()
